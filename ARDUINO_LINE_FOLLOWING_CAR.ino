d1845d215ad62ab0116df88779ad567b59e04937
Mahnoor241 <62926834+Mahnoor241@users.noreply.github.com>
2021-04-22 13:33:52 +0500

    Update ARDUINO_LINE_FOLLOWING_CAR.ino

--- ARDUINO_LINE_FOLLOWING_CAR.ino
+++ ARDUINO_LINE_FOLLOWING_CAR.ino
@@ -15,7 +15,7 @@ AF_DCMotor motor2(2, MOTOR12_1KHZ);
 AF_DCMotor motor3(3, MOTOR34_1KHZ);
 AF_DCMotor motor4(4, MOTOR34_1KHZ);
 
-
+//Start
 void setup() {
   //declaring pin types
   pinMode(left,INPUT);
